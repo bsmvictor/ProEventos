47fba5d46ebccda3b6997986d77f18e93e2ad64a
Bsmvictor <victor.boaventura@gec.inatel.br>
2025-03-13 14:31:00 -0300

    chore: criando um novo evento no banco de dados

--- Back/src/ProEventos.API/Data/DataContext.cs
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -7,10 +7,13 @@ using ProEventos.API.Models;
 
 namespace ProEventos.API.Data
 {
+    // DataContext é a classe que representa o banco de dados
     public class DataContext : DbContext
     { 
+        // DataContext é um construtor que recebe um objeto do tipo DbContextOptions<DataContext> e passa para a classe base
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         
+        // Eventos é uma propriedade do tipo DbSet<Evento> que representa a tabela Eventos no banco de dados
         public DbSet<Evento> Eventos { get; set; }
     }
 
